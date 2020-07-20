5fc890b052044f451d667fdd7b15cc076bd5b3f7
Studio Phoenix <Foxstrock@gmail.com>
2020-07-20 21:39:54 +0200

    Ao

--- dove_siamo.php
+++ dove_siamo.php
@@ -5,4 +5,12 @@
 
     echo base64_decode($_SESSION['impostazioni']['htmlDoveSiamo']);
 
+    ?>
+
+    <div class="google-maps">
+        <iframe src="https://goo.gl/maps/TR45AdB78szdMEdo6" allowfullscreen></iframe>
+    </div>
+
+<?php
+
     include 'footer.php';
